cd2c625bb393a34ab4c187671732f4a2c411ff9a
Ondřej Košarko <kosarko@ufal.mff.cuni.cz>
2015-01-14 17:45:03 +0100

    merging...

--- dspace-api/src/main/java/cz/cuni/mff/ufal/Report.java
+++ dspace-api/src/main/java/cz/cuni/mff/ufal/Report.java
@@ -11,7 +11,6 @@ import java.io.StringWriter;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
